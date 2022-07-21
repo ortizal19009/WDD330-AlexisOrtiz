abc02b9d0cfb3d88bd9325817d6ca9d26338c085
Alexis Ortiz <83848928+ortizal19009@users.noreply.github.com>
2022-07-21 03:28:34 -0500

    updated

--- FinalProject/main.js
+++ FinalProject/main.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function () {
   const ApiKeyPublic = "32e24560c577fa69e6a5da0dc002dc9e";
   const ApiKeyPrivate = "a1cadff6ed4ad893877099254f228306de99e67b";
@@ -15,8 +14,16 @@ $(document).ready(function () {
       let listMarvel = "";
       let listNames = "";
       let arrayMarvel = response.data.results;
-      console.log(arrayMarvel);
       for (let movie of arrayMarvel) {
+          let i="";
+          let datos = "";
+          i = movie.series.items;    
+          for ( let x of i){
+            console.log(x.name);
+            datos = x.name;
+        }
+          /* console.log(i); */
+
         listMarvel += `
         <div class="list col-sm-6">
         <div class="card my-1">
@@ -27,9 +34,36 @@ $(document).ready(function () {
           <p class="card-text">${movie.description}</p>
         </div>
       </div>
+      <!-- Modal -->
+            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+              <div class="modal-dialog">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Extra information</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                  </div>
+                  <div class="modal-body">
+                   <p>
+                   ${datos}
+                   </p> 
+        
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+      
+      <!-- Button trigger modal -->
+      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        Show information
+      </button>
         </div>
       </div>
       `;
+      //console.log(datos);
       }
       $("#listaMarvel").html(listMarvel);
 
@@ -43,14 +77,14 @@ $(document).ready(function () {
 
       $("#findData").keypress(function () {
         let names = $("#findData").val();
-        function foundHero(hero){
-            return hero.name === names;
+        function foundHero(hero) {
+          return hero.name === names;
         }
         if (names != "") {
-            let hero = arrayMarvel.find(foundHero);
-            console.log(hero);
-            let datosHero = `
-            <div class="list col-sm-6">
+          let hero = arrayMarvel.find(foundHero);
+          console.log(hero);
+          let datosHero = `
+            <div class="list col-sm-4">
             <div class="card my-1">
             <div class="card" style="width: 5rem;">
             <img src="${hero.thumbnail.path}.${hero.thumbnail.extension}" class="card-img-top" alt="...">
@@ -68,6 +102,13 @@ $(document).ready(function () {
           location.reload();
         }
       });
+      $(".footerInformation").html(
+        `<div>
+            <p>${response.copyright}</p>
+            <p>${response.attributionText}</p>
+        </div>
+        `
+      );
     })
     .catch((error) => console.log("Hay un error" + error));
 });
